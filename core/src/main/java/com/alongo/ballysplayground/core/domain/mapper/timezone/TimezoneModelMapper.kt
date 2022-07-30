e367f92dfd8997d6cad9f1205d0597c168f926fe
Vladimir Vetrov <pdastore1@gmail.com>
2022-07-30 23:07:11 +0300

    Add class TimezoneModelMapper

---
+++ core/src/main/java/com/alongo/ballysplayground/core/domain/mapper/timezone/TimezoneModelMapper.kt
@@ -0,0 +1,9 @@
+package com.alongo.ballysplayground.core.domain.mapper.timezone
+
+import com.alongo.ballysplayground.core.domain.entity.networking.Timezone
+import com.alongo.ballysplayground.core.domain.entity.networking.TimezoneDto
+import com.alongo.ballysplayground.core.domain.mapper.DtoModelMapper
+
+class TimezoneModelMapper : DtoModelMapper<TimezoneDto, Timezone>() {
+    override fun fromDtoToModel(dto: TimezoneDto) = Timezone(dto.timezone, dto.name)
+}
