6e0da6c6604324152e195409125bd834e3e1f7d9
Vladimir Vetrov <pdastore1@gmail.com>
2022-07-30 23:19:35 +0300

    Mark fromModelToDto as open

--- core/src/main/java/com/alongo/ballysplayground/core/domain/mapper/DtoModelMapper.kt
+++ core/src/main/java/com/alongo/ballysplayground/core/domain/mapper/DtoModelMapper.kt
@@ -1,6 +1,6 @@
 package com.alongo.ballysplayground.core.domain.mapper
 
 abstract class DtoModelMapper<T, R> {
-    fun fromModelToDto(model: R): T = throw NotImplementedError("fromModelToDto in ${this.javaClass.simpleName} has not been implemented")
+    open fun fromModelToDto(model: R): T = throw NotImplementedError("fromModelToDto in ${this.javaClass.simpleName} has not been implemented")
     abstract fun fromDtoToModel(dto: T): R
 }
