7701d0245e97bdb5201ff5118e9ca10d5dd9c749
vlavet <pdastore1@gmail.com>
2022-07-30 21:43:19 +0300

    Add Design System module

---
+++ design_system/src/main/java/com/alongo/design_system/presentation/AppTextField.kt
@@ -0,0 +1,18 @@
+package com.alongo.design_system.presentation
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun AppTextField(value: String, onValueChange: (String) -> Unit, modifier: Modifier = Modifier) {
+    TextField(value = value, onValueChange = onValueChange, modifier = modifier.fillMaxWidth())
+}
+
+@Composable
+@Preview
+fun AppTextField_Preview() {
+    AppTextField("Example", onValueChange = {})
+}
