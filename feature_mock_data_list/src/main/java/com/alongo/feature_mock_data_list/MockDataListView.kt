34b9e66184af891ed2d37962fbc0b175594a0c79
vlavet <pdastore1@gmail.com>
2022-07-30 18:16:16 +0300

    Implement data creation interface

--- feature_mock_data_list/src/main/java/com/alongo/feature_mock_data_list/MockDataListView.kt
+++ feature_mock_data_list/src/main/java/com/alongo/feature_mock_data_list/MockDataListView.kt
@@ -1,24 +1,50 @@
 package com.alongo.feature_mock_data_list
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 fun MockDataListView(viewModel: MockDataListViewModel) {
     when (val state = viewModel.state.value) {
-        is MockDataListState.DataReady -> {
+        is MockDataListViewModel.State.DataReady -> {
             LazyColumn {
-                items(state.data) { item->
-                    Text(text = item.text)
+                items(state.data) { item ->
+                    TextField(value = item.text, onValueChange = { newValue ->
+                        viewModel.handleEvent(
+                            MockDataListViewModel.Event.OnDataUpdated(
+                                item,
+                                newValue
+                            )
+                        )
+                    })
+                }
+
+                item {
+                    Button(onClick = { viewModel.handleEvent(MockDataListViewModel.Event.OnDataCreated) }) {
+                        Text(text = "Create Data")
+                    }
                 }
             }
         }
 
-        is MockDataListState.LoadingData -> {
-            CircularProgressIndicator()
+        is MockDataListViewModel.State.LoadingData -> {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                CircularProgressIndicator()
+            }
         }
+    }
+
+    LaunchedEffect(key1 = Unit) {
+        viewModel.handleEvent(MockDataListViewModel.Event.RequestData)
     }
 }
