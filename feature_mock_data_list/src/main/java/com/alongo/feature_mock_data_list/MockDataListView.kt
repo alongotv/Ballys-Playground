e9892c8d229f241fcbe6ba9c226d04dbe4da4d8e
Vladimir Vetrov <pdastore1@gmail.com>
2022-07-31 01:53:50 +0300

    Add SwipeToDismiss

--- feature_mock_data_list/src/main/java/com/alongo/feature_mock_data_list/MockDataListView.kt
+++ feature_mock_data_list/src/main/java/com/alongo/feature_mock_data_list/MockDataListView.kt
@@ -2,12 +2,18 @@ package com.alongo.feature_mock_data_list
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.DismissDirection
+import androidx.compose.material.DismissState
+import androidx.compose.material.DismissValue
 import androidx.compose.material.Divider
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.SwipeToDismiss
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -17,21 +23,41 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.alongo.design_system.presentation.AppTextField
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun MockDataListView(viewModel: MockDataListViewModel) {
     when (val state = viewModel.state.value) {
         is MockDataListViewModel.State.DataReady -> {
-            LazyColumn(modifier = Modifier.padding(8.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
                 items(state.data) { item ->
-                    AppTextField(value = item.text, onValueChange = { newValue ->
-                        viewModel.handleEvent(
-                            MockDataListViewModel.Event.OnDataUpdated(
-                                item,
-                                newValue
-                            )
+                    val dismissState = DismissState(initialValue = DismissValue.Default,
+                        confirmStateChange = {
+                            println(it)
+                            if (it != DismissValue.Default)
+                                viewModel.handleEvent(MockDataListViewModel.Event.OnDataDismissed(item))
+                            false
+                        }
+                    )
+                    SwipeToDismiss(
+                        state = dismissState, background = {}, directions = setOf(
+                            DismissDirection.EndToStart
                         )
-                    })
-                    Divider(color = Color.Black)
+                    ) {
+                        AppTextField(value = item.text, onValueChange = { newValue ->
+                            viewModel.handleEvent(
+                                MockDataListViewModel.Event.OnDataUpdated(
+                                    item,
+                                    newValue
+                                )
+                            )
+                        }, placeholder = { Text(text = "Enter text...") })
+                        Divider(color = Color.Black)
+                    }
                 }
 
                 item {
