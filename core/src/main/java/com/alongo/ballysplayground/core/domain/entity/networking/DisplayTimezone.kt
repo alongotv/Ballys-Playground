7ceb53fb41143de7b89a9c14915e82927a08a6a7
Vladimir Vetrov <pdastore1@gmail.com>
2022-07-30 23:58:13 +0300

    Add currentTime field

--- core/src/main/java/com/alongo/ballysplayground/core/domain/entity/networking/DisplayTimezone.kt
+++ core/src/main/java/com/alongo/ballysplayground/core/domain/entity/networking/DisplayTimezone.kt
@@ -1,3 +1,3 @@
 package com.alongo.ballysplayground.core.domain.entity.networking
 
-data class DisplayTimezone(val name: String, val time: String)
+data class DisplayTimezone(val name: String, val timezone: String, val currentTime: String = "")
