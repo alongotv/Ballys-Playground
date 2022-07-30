8be18d6d5af2e88baa148d884c18934ce1daf6f2
Vladimir Vetrov <pdastore1@gmail.com>
2022-07-30 23:14:32 +0300

    Add Timezone to list of AppDatabase entities

--- core/src/main/java/com/alongo/ballysplayground/core/data/datasource/database/AppDatabase.kt
+++ core/src/main/java/com/alongo/ballysplayground/core/data/datasource/database/AppDatabase.kt
@@ -5,8 +5,9 @@ import androidx.room.RoomDatabase
 import com.alongo.ballysplayground.core.data.datasource.database.mock.MockDataDao
 import com.alongo.ballysplayground.core.data.datasource.database.timezone.TimezoneDao
 import com.alongo.ballysplayground.core.domain.entity.database.MockData
+import com.alongo.ballysplayground.core.domain.entity.database.Timezone
 
-@Database(entities = [MockData::class], version = 1)
+@Database(entities = [MockData::class, Timezone::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun mockDataDao(): MockDataDao
     abstract fun timezoneDao(): TimezoneDao
