7d2cea663700dea839a70dcee487500a26de51b6
vlavet <pdastore1@gmail.com>
2022-07-30 21:43:44 +0300

    Add divider, padding to MockDataListView

--- feature_mock_data_list/src/main/java/com/alongo/feature_mock_data_list/MockDataListView.kt
+++ feature_mock_data_list/src/main/java/com/alongo/feature_mock_data_list/MockDataListView.kt
@@ -2,25 +2,28 @@ package com.alongo.feature_mock_data_list
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Divider
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.alongo.design_system.presentation.AppTextField
 
 @Composable
 fun MockDataListView(viewModel: MockDataListViewModel) {
     when (val state = viewModel.state.value) {
         is MockDataListViewModel.State.DataReady -> {
-            LazyColumn {
+            LazyColumn(modifier = Modifier.padding(8.dp), horizontalAlignment = Alignment.CenterHorizontally) {
                 items(state.data) { item ->
-                    TextField(value = item.text, onValueChange = { newValue ->
+                    AppTextField(value = item.text, onValueChange = { newValue ->
                         viewModel.handleEvent(
                             MockDataListViewModel.Event.OnDataUpdated(
                                 item,
@@ -28,6 +31,7 @@ fun MockDataListView(viewModel: MockDataListViewModel) {
                             )
                         )
                     })
+                    Divider(color = Color.Black)
                 }
 
                 item {
