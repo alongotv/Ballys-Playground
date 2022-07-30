2522ecc95e8f877e83d4da3a975feabcb4dd738d
Vladimir Vetrov <pdastore1@gmail.com>
2022-07-30 23:06:47 +0300

    Annotate provideTimezoneApi with @Provides

--- core/src/main/java/com/alongo/ballysplayground/core/di/data/datasource/networking/TimezoneApiModule.kt
+++ core/src/main/java/com/alongo/ballysplayground/core/di/data/datasource/networking/TimezoneApiModule.kt
@@ -2,6 +2,7 @@ package com.alongo.ballysplayground.core.di.data.datasource.networking
 
 import com.alongo.ballysplayground.core.data.datasource.networking.timezone.TimezoneApi
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
@@ -9,5 +10,6 @@ import retrofit2.Retrofit
 @Module
 @InstallIn(SingletonComponent::class)
 class TimezoneApiModule {
+    @Provides
     fun provideTimezoneApi(retrofit: Retrofit) = retrofit.create(TimezoneApi::class.java)
 }
