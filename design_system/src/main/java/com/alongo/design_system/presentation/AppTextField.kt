63db94d16a37502792f01cba506694be34314351
Vladimir Vetrov <pdastore1@gmail.com>
2022-07-31 01:53:11 +0300

    Add placeholder text to AppTestField

--- design_system/src/main/java/com/alongo/design_system/presentation/AppTextField.kt
+++ design_system/src/main/java/com/alongo/design_system/presentation/AppTextField.kt
@@ -7,8 +7,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
-fun AppTextField(value: String, onValueChange: (String) -> Unit, modifier: Modifier = Modifier) {
-    TextField(value = value, onValueChange = onValueChange, modifier = modifier.fillMaxWidth())
+fun AppTextField(value: String, onValueChange: (String) -> Unit, modifier: Modifier = Modifier, placeholder: @Composable () -> Unit = {}) {
+    TextField(value = value, onValueChange = onValueChange, modifier = modifier.fillMaxWidth(), placeholder = {
+        placeholder()
+    })
 }
 
 @Composable
