ec79eda45a7d1fa686c89a3766aeb45bd7da9054
Vladimir Vetrov <pdastore1@gmail.com>
2022-07-30 23:56:51 +0300

    Fix mappers

--- core/src/main/java/com/alongo/ballysplayground/core/domain/mapper/timezone/TimezoneDisplayMapper.kt
+++ core/src/main/java/com/alongo/ballysplayground/core/domain/mapper/timezone/TimezoneDisplayMapper.kt
@@ -5,5 +5,5 @@ import com.alongo.ballysplayground.core.domain.entity.database.Timezone
 import com.alongo.ballysplayground.core.domain.mapper.ModelDisplayMapper
 
 class TimezoneDisplayMapper : ModelDisplayMapper<Timezone, DisplayTimezone>() {
-    override fun fromModelToDisplay(model: Timezone) = DisplayTimezone(model.name, model.timezone)
+    override fun fromModelToDisplay(model: Timezone) = DisplayTimezone(name = model.name, timezone = model.timezone)
 }
--- core/src/main/java/com/alongo/ballysplayground/core/domain/mapper/timezone/TimezoneModelMapper.kt
+++ core/src/main/java/com/alongo/ballysplayground/core/domain/mapper/timezone/TimezoneModelMapper.kt
@@ -5,5 +5,5 @@ import com.alongo.ballysplayground.core.domain.entity.networking.TimezoneDto
 import com.alongo.ballysplayground.core.domain.mapper.DtoModelMapper
 
 class TimezoneModelMapper : DtoModelMapper<TimezoneDto, Timezone>() {
-    override fun fromDtoToModel(dto: TimezoneDto) = Timezone(name = dto.timezone, timezone = dto.name)
+    override fun fromDtoToModel(dto: TimezoneDto) = Timezone(name = dto.name, timezone = dto.timezone)
 }
